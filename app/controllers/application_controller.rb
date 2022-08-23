5a526a80f7eefc9043c036b9850d314bdd599ba8
Lukas Mills <lukascodes64@gmail.com>
2022-08-23 09:37:57 -0400

    Fixed controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,19 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+
+   get "/" do
+     movies = Movie.all
+     movies.to_json(include: { reviews: {include: :user} })     
+   end
+
+
+
+
+
   get "/movies" do
     movies = Movie.all
-    movies.to_json
+    movies.to_json(include: :reviews)
   end
 
   get "/reviews" do 
     reviews = Review.all
-    reviews.to_json
+    reviews.to_json(include: :user)
   end
 
   get "/users" do 
     users = User.all
     users.to_json
   end
+
+   delete "/movies/:id" do
+     movies = Movie.find(params[:id])
+     movies.destroy
+     movies.to_json
+   end
+
+  
+
 
 end
